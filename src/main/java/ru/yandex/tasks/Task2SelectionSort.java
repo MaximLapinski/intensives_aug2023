23ee3647c411790af7d01b6e9a45bfdf72f049e1
maximlapinskiy <me@lapinskij.ru>
2023-08-14 19:02:37 +0300

    "Second task"

--- src/main/java/ru/yandex/tasks/Task2SelectionSort.java
+++ src/main/java/ru/yandex/tasks/Task2SelectionSort.java
@@ -2,12 +2,27 @@ package ru.yandex.tasks;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Task2SelectionSort {
     public static ArrayList<Integer> sort(ArrayList<Integer> numbers) {
-        Integer[] mas = new Integer[numbers.size()];
-        numbers.toArray(mas);
-        return null;
+        Integer[] array = new Integer[numbers.size()];
+        numbers.toArray(array);
+        for (int i = 0; i < array.length-1; i++) { // внешний обычный  цикл
+            int min = i;
+
+            for (int j = i + 1; j < array.length; j++) { // обычный цикл, но с отчетом с сортированных чисел
+                if (array[j] < array[min]) {
+                    min = j;
+                }
+            }
+            int temp = array[i];     // вставка отссортиованного числа, в положеную ему ячейку
+            array[i] = array[min];
+            array[min] = temp;
+        }
+        numbers = new ArrayList<>();
+        Collections.addAll(numbers, array);
+        return numbers;
     }
 
     public static void selfCheck() {
